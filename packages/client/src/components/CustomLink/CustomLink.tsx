d81f230161e20438375a711a772123ddddb4359c
an_bir <an_bir@mail.com>
2022-12-23 23:11:02 +0300

    mod

--- packages/client/src/components/CustomLink/CustomLink.tsx
+++ packages/client/src/components/CustomLink/CustomLink.tsx
@@ -2,12 +2,12 @@ import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 import classes from './CustomLink.module.scss';
 
-type Props = {
+type T_Props = {
   to: string;
   title: string;
 };
 
-export const CustomLink: FC<Props> = ({ to, title }) => {
+export const CustomLink: FC<T_Props> = ({ to, title }) => {
   return (
     <Link className={classes.root} to={to}>
       {title}
