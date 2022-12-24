04ad2fa634f72d1c3bce654f7ec04193adbaece5
an_bir <an_bir@mail.com>
2022-12-24 21:19:20 +0300

    modified

--- packages/client/src/pages/SigninPage/SigninPage.tsx
+++ packages/client/src/pages/SigninPage/SigninPage.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import { FormBuilder, T_FormStructure, getFormFields } from '../../modules/formBuilder';
+import React, { FC, useState } from 'react';
+import { FormBuilder, T_FormFieldNames, T_FormStructure, getFormFields } from '../../modules/formBuilder';
 import { Box } from '@mui/material';
 import { yup } from '../../modules/formBuilder/constants/validation';
 import classes from './SigninPage.module.scss';
@@ -7,11 +7,14 @@ import { T_SigninData } from '../../global/types';
 import { authApi } from '../../api';
 import { useNavigate } from 'react-router-dom';
 import { useNotification } from '../../global/hooks';
+import { UseFormReturn } from 'react-hook-form';
+
+const FIELDS: T_FormFieldNames = ['login', 'password'];
 
 const getFormStructure = (): T_FormStructure => {
   return {
     title: 'Вход',
-    fields: getFormFields(['login', 'password']),
+    fields: getFormFields(FIELDS),
     link: {
       to: '/signup',
       title: 'Нет аккаунта?',
@@ -32,20 +35,37 @@ type T_Schema = typeof validationSchema;
 export const SigninPage: FC = () => {
   const navigate = useNavigate();
   const { showAlert } = useNotification();
+  const [formApi, setFormApi] = useState<UseFormReturn | null>();
+
+  const setFieldErrors = () => {
+    FIELDS.forEach((name) => formApi?.setError(name, {}));
+  };
 
   const onSubmit = async (data: T_SigninData) => {
     try {
       await authApi.signin(data);
       navigate('/');
     } catch (e) {
-      if (e instanceof Error && showAlert) showAlert(e.message);
+      if (e instanceof Error && showAlert) {
+        showAlert(e.message);
+        setFieldErrors();
+      }
     }
+  };
+
+  const getFormApi = (api: UseFormReturn) => {
+    if (!formApi) setFormApi(api);
   };
 
   return (
     <Box className={classes.root}>
       <Box className={classes.root__formWrapper}>
-        <FormBuilder<T_SigninData, T_Schema> onSubmit={onSubmit} structure={getFormStructure()} validationSchema={validationSchema} />
+        <FormBuilder<T_SigninData, T_Schema>
+          onSubmit={onSubmit}
+          structure={getFormStructure()}
+          validationSchema={validationSchema}
+          getFormApi={getFormApi}
+        />
       </Box>
     </Box>
   );
--- packages/client/src/pages/SignupPage/SignupPage.tsx
+++ packages/client/src/pages/SignupPage/SignupPage.tsx
@@ -1,16 +1,19 @@
-import React, { FC } from 'react';
-import { FormBuilder, T_FormStructure, yup, getFormFields } from '../../modules/formBuilder';
+import React, { FC, useState } from 'react';
+import { FormBuilder, T_FormStructure, yup, getFormFields, T_FormFieldNames } from '../../modules/formBuilder';
 import { Box } from '@mui/material';
 import classes from './Signup.module.scss';
 import { T_SignupData } from '../../global/types';
 import { authApi } from '../../api';
 import { useNavigate } from 'react-router-dom';
 import { useNotification } from '../../global/hooks';
+import { UseFormReturn } from 'react-hook-form';
+
+const FIELDS: T_FormFieldNames = ['email', 'login', 'first_name', 'second_name', 'phone', 'password', 'confirmPassword'];
 
 const getFormStructure = (): T_FormStructure => {
   return {
     title: 'Регистрация',
-    fields: getFormFields(['email', 'login', 'first_name', 'second_name', 'phone', 'password', 'confirmPassword']),
+    fields: getFormFields(FIELDS),
     link: {
       to: '/signin',
       title: 'Войти',
@@ -36,20 +39,37 @@ type T_Schema = typeof validationSchema;
 export const SignupPage: FC = () => {
   const navigate = useNavigate();
   const { showAlert } = useNotification();
+  const [formApi, setFormApi] = useState<UseFormReturn | null>();
+
+  const setFieldErrors = () => {
+    FIELDS.forEach((name) => formApi?.setError(name, {}));
+  };
 
   const onSubmit = async (data: T_SignupData) => {
     try {
       await authApi.signup(data);
       navigate('/');
     } catch (e) {
-      if (e instanceof Error && showAlert) showAlert(e.message);
+      if (e instanceof Error && showAlert) {
+        showAlert(e.message);
+        setFieldErrors();
+      }
     }
+  };
+
+  const getFormApi = (api: UseFormReturn) => {
+    if (!formApi) setFormApi(api);
   };
 
   return (
     <Box className={classes.root}>
       <Box className={classes.root__formWrapper}>
-        <FormBuilder<T_SignupData, T_Schema> onSubmit={onSubmit} structure={getFormStructure()} validationSchema={validationSchema} />
+        <FormBuilder<T_SignupData, T_Schema>
+          onSubmit={onSubmit}
+          structure={getFormStructure()}
+          validationSchema={validationSchema}
+          getFormApi={getFormApi}
+        />
       </Box>
     </Box>
   );
