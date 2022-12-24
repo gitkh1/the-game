6a6742cb3bbda051aefa2c1284690edbced9f087
Anton Makarenko <5018813+ftoh@users.noreply.github.com>
2022-12-24 22:04:11 +0700

    Игровой движок - Демо (#5)
    * initial commit
    
    * temp
    
    * add semicolons and remove unnecessary type in Game.tsx
    
    * add semicolons to missed files
    
    * fix lint
    
    Co-authored-by: FTOH <FTOH@users.noreply.github.com>

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './main.scss';
-import './index.scss'
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
