eb8450d1567ba2f2bc409238f6dc87add9b5f766
Владимир Филимоненко <zrnrd@mail.ru>
2023-01-13 15:22:45 +0500

    Добавлена смена полноэкранного режима в настройках

--- packages/client/src/pages/MainMenuSettings/MainMenuSettings.tsx
+++ packages/client/src/pages/MainMenuSettings/MainMenuSettings.tsx
@@ -2,8 +2,34 @@ import classes from './MainMenuSettings.module.scss';
 import Button from '@mui/material/Button';
 import mainPageBG from '../../assets/images/game-main-menu-bg.jpg';
 import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+function toggleFullscreen(e) {
+    const elem = document.documentElement;
+    if (!document.fullscreenElement) {
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      }
+      e.target.textContent = 'Полноэкранный';
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+      e.target.textContent = 'Оконный';
+    }
+}
 
 export default function MainMenuSettings() {
+    const [windowMode, setWindowMode] = useState('Оконный');
+
+    useEffect(() => {
+        if (!document.fullscreenElement) {
+            setWindowMode('Оконный');
+          } else {
+            setWindowMode('Полноэкранный');
+          }
+    }, []);
+
     return(
         <div className={ classes.container }>
             <img src={mainPageBG} alt="game-main-menu-background" className={ classes.background }/>
@@ -13,7 +39,7 @@ export default function MainMenuSettings() {
                     <div className={ classes.settings__description }>
                         Режим экрана:
                     </div>
-                    <Button color = "primary" variant="contained">Оконный</Button>
+                    <Button color = "primary" variant="contained" onClick={ toggleFullscreen }>{ windowMode }</Button>
                 </div>
                 <NavLink to='/main-menu'>
                     <Button color = "primary" variant="contained">Вернуться в меню</Button>
