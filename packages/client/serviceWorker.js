c21ebead3219a312620ea30f783ee9f1e995a4cf
Владимир Филимоненко <zrnrd@mail.ru>
2023-01-14 14:41:16 +0500

    Создан serviceWorker.js

---
+++ packages/client/serviceWorker.js
@@ -0,0 +1,44 @@
+const staticAssets = [
+  '/',
+  '/index.html',
+  '/src/main.scss',
+  '/src/app.tsx',
+];
+
+const STATIC_CACHE_NAME = 'static-data';
+const DYNAMIC_CACHE_NAME = 'dynamic-data';
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(STATIC_CACHE_NAME);
+  console.log('install');
+  cache.addAll(staticAssets);
+});
+
+self.addEventListener('activate', e => {
+  console.log('activate');
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const { request } = event;
+  event.respondWith(cacheData(request));
+});
+
+async function cacheData(request) {
+  const cashedRequest = await caches.match(request);
+  if (staticAssets.some(sa => request.url.indexOf(sa) >= 0) || request.headers.get('accept').includes('text/html')) {
+    return cashedRequest || await caches.match('/offline.html') || networkFirst(request);
+  }
+  return cashedRequest || networkFirst(request);
+}
+
+async function networkFirst(request) {
+  const cache = await caches.open(DYNAMIC_CACHE_NAME);
+  try {
+    const response = await fetch(request);
+    cache.put(request, response.clone());
+    return response;
+  } catch (error) {
+    return await cache.match(request);
+  }
+};
