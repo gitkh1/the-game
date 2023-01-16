8f06eac09a5821147fba9e60aeef066bb37079da
Aleksei <i-ees@ya.ru>
2023-01-16 19:25:21 +1000

    bug fix

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 
 const appContent = '404';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve('hey') }));
 
