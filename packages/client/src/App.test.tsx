e0163e57608144639c78a56fd6a2fa7bd52b67f3
Aleksei <i-ees@ya.ru>
2023-01-16 19:05:42 +1000

    bug fix

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,12 +1,20 @@
 import App from './App';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { ErrorBoundary } from './modules/ErrorBoundary/ErrorBoundary';
 
 const appContent = '404';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve('hey') }));
 
 test('Example test', async () => {
-  render(<App />);
+  render(<React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+  );
   expect(screen.findAllByText(appContent)).toBeDefined();
 });
