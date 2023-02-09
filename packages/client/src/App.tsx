e63c4d359d9ede4f2fb3d613a2a5382562ff477b
Aleksei <104002943+gitkh1@users.noreply.github.com>
2023-02-09 16:43:54 +1000

    Update App.tsx
    delete ts-ignore

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
