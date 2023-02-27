1656852c21159838e3ada4d63cdb3fe55b53f83d
Aleksei <i-ees@ya.ru>
2023-02-27 23:39:53 +1000

    Исправлены замечания

--- packages/server/db.ts
+++ packages/server/db.ts
@@ -1,9 +1,9 @@
 import { Sequelize, SequelizeOptions } from "sequelize-typescript";
 
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } = process.env;
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT, POSTGRES_HOST } = process.env;
 
 const sequelizeOptions: SequelizeOptions = {
-  host: "postgres",
+  host: POSTGRES_HOST,
   port: Number(POSTGRES_PORT),
   username: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
